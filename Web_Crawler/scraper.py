9697d739ee3e41ad2ccbbd8e877785e34ffa5f35
Charles Llewellyn <cml176@txstate.edu>
2017-10-01 14:50:53 -0500

    Update scraper.py
    What this does (so far):
    Extracts recipe name, cook time, prep time, total time and ingredients from a recipe page on (www.allrecipes.com)
    
    What still needs to be done:
    extract cooking directions from (www.allrecipes.com), crawl allrecipes.com for more recipes, export extracted data into CSV file with proper fields. 
    
    Changes: Added comments for better understanding

--- Web_Crawler/scraper.py
+++ Web_Crawler/scraper.py
@@ -2,31 +2,31 @@ from bs4 import BeautifulSoup  #html parser
 import requests #URL navigator
 
 
-HEADERS = {
+HEADERS = {   #some websites require a user agent. This is here in case it is needed
    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 }
 
-ingredients = []
-r = requests.get("http://allrecipes.com/recipe/73350/shrimp-burritos")
+ingredients = [] #ingredients are extracted into array (line 23)
+r = requests.get("http://allrecipes.com/recipe/73350/shrimp-burritos") #grabs contents from given URL
 
 
-if (r.status_code == 200):
-    content = r.content
-    soup = BeautifulSoup(content, "html.parser")
+if (r.status_code == 200):   #if status_code == 200 then the request was pulled with no errors
+    content = r.content      #grabs the contents of URL
+    soup = BeautifulSoup(content, "html.parser")   #parses contents into readable format
 
-    name = soup.find('h1').get_text()
-    totalTime = soup.find('span', {'class': 'ready-in-time'}).get_text()
-    cookTime = soup.find('time', {'itemprop': 'cookTime'}).get_text()
-    prepTime = soup.find('time', {'itemprop': 'prepTime'}).get_text()
-    ingredients_list = soup.findAll('span', {'itemprop': 'ingredients'})
-    for x in ingredients_list:
-        ingredients.append(x.text)
-        x.next_sibling
+    name = soup.find('h1').get_text()   #extracts the Recipe Name
+    totalTime = soup.find('span', {'class': 'ready-in-time'}).get_text()   #extracts the total time for recipe
+    cookTime = soup.find('time', {'itemprop': 'cookTime'}).get_text()      #extracts the cook time for recipe
+    prepTime = soup.find('time', {'itemprop': 'prepTime'}).get_text()      #extracts the prep time for recipe
+    ingredients_list = soup.findAll('span', {'itemprop': 'ingredients'})   #extracts the ingredients (w/ html tags) into a list
+    for x in ingredients_list:       #traverses list
+        ingredients.append(x.text)   #extracts text from <html tag> and appends to ingredients list
+        x.next_sibling   #jumps to next <span> html tag
     instructions = soup.findAll('span', {'class': 'recipe-directions_list--item'})
 
 else:
-    print("Error! Bad URL!")
-    exit()
+    print("Error! Bad URL!")   #if status_code != 200 then request pulled with errors
+    exit()   #exit program
 
 
 print("title: ", name)
@@ -36,7 +36,7 @@ print("Cook Time: ", cookTime)
 print("Ingredients: ", end = '')
 for item in ingredients:
     print (item, sep = '', end =',', flush = True)
-print("\nInstructions: ", instructions)
+print("\nInstructions: ", instructions)   #*Instructions are not being pulled properly from HTML***FIX THIS***
 
 
 
