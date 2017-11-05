2c7cdb73bad318a7c3fc169213850dd28d087d67
aMorneault <31698116+aMorneault@users.noreply.github.com>
2017-11-04 22:59:17 -0500

    GUI 2.0
    This will be the GUI we will use from here on out.  Refer to README.txt for info.

---
+++ PantryPlanner/src/pantryPlanner/views/recipeMenu.java
@@ -0,0 +1,117 @@
+package pantryPlanner.views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EtchedBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class recipeMenu extends JFrame 
+{
+
+	private JPanel ctpRecipe;
+	private JButton b_add;
+	private JButton b_remove;
+	private JButton b_search;
+	private JButton b_return;
+
+
+
+	/**
+	 * Create the recipeMenu frame.
+	 */
+	public recipeMenu() 
+	{
+		initComponentsRecipe();
+		createEventsRecipe();
+	}
+
+    //////////////////////////////////////////////////////////////////////////
+    // initComponents: has all code for creating and initializing components
+    //////////////////////////////////////////////////////////////////////////
+	private void initComponentsRecipe() 
+	{
+		setIconImage(Toolkit.getDefaultToolkit().getImage(recipeMenu.class.getResource("/pantryPlanner/resources/recipe32.png")));
+		setTitle("RECIPE MENU");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		ctpRecipe = new JPanel();
+		ctpRecipe.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		setContentPane(ctpRecipe);
+		
+		JLabel l_RecipeGreeting = new JLabel("");
+		l_RecipeGreeting.setIcon(new ImageIcon(recipeMenu.class.getResource("/pantryPlanner/resources/recipeBook128.png")));
+		
+		b_add = new JButton("ADD RECIPE");
+		
+		b_remove = new JButton("REMOVE RECIPE");
+		
+		b_search = new JButton(" SEARCH RECIPE");
+		
+		b_return = new JButton("RETURN");
+
+		GroupLayout gl_ctpRecipe = new GroupLayout(ctpRecipe);
+		gl_ctpRecipe.setHorizontalGroup(
+			gl_ctpRecipe.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_ctpRecipe.createSequentialGroup()
+					.addContainerGap(143, Short.MAX_VALUE)
+					.addComponent(l_RecipeGreeting, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
+					.addGap(141))
+				.addGroup(gl_ctpRecipe.createSequentialGroup()
+					.addGap(57)
+					.addGroup(gl_ctpRecipe.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(b_add, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(b_remove, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED, 97, Short.MAX_VALUE)
+					.addGroup(gl_ctpRecipe.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(b_search, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(b_return, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addContainerGap(92, Short.MAX_VALUE))
+		);
+		gl_ctpRecipe.setVerticalGroup(
+			gl_ctpRecipe.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_ctpRecipe.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(l_RecipeGreeting)
+					.addPreferredGap(ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+					.addGroup(gl_ctpRecipe.createParallelGroup(Alignment.BASELINE)
+						.addComponent(b_add)
+						.addComponent(b_search))
+					.addGap(18)
+					.addGroup(gl_ctpRecipe.createParallelGroup(Alignment.BASELINE)
+						.addComponent(b_remove)
+						.addComponent(b_return))
+					.addContainerGap())
+		);
+		ctpRecipe.setLayout(gl_ctpRecipe);
+		setVisible(true);
+		
+	}	
+	
+	//////////////////////////////////////////////////////////////////////////
+	// createEvents: has all code for creating events
+    //////////////////////////////////////////////////////////////////////////
+	private void createEventsRecipe() 
+	{
+		b_return.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				dispose();
+			}
+		});
+		
+	}
+}// end class recipeMenu
---
+++ README.txt
@@ -0,0 +1,17 @@
+README.txt for Team Mocha GUI for main menu, pantry menu, recipe menu
+
+*********************************************************************************************************************************
+************************************************ UPDATE - 04NOV2017 *************************************************************
+*********************************************************************************************************************************
+
+I completely restructured the code for the GUI.  I have now added three classes to the src under pantryPlanner they are as follows: common: this is where code that is not GUI related goes.  views: this is where the GUI is located.  resources: this is where you put .png for logos or other resources to keep all data enclosed in the app.  This should make the integration of the recipe and pantry databases easier.
+
+*********************************************************************************************************************************
+
+
+*****TODO: The Group needs to decide if the login is going to be the first thing that is done and "driving the boat" for the app.
+********** Or do we allow some functionality while not having a user signed in?
+
+*****TODO: Need to link both the recipe/pantry menu buttons to desired actions with the databases
+
+
