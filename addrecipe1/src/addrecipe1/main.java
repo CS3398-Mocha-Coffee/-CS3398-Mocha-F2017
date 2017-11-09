94895517469db27143e454ec82de6552715cf358
iabelh <31448919+iabelh@users.noreply.github.com>
2017-11-08 22:18:00 -0600

    addRecipe feature

---
+++ addrecipe1/src/addrecipe1/main.java
@@ -0,0 +1,13 @@
+package addrecipe1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  addRecipe test1 = new addRecipe();
+	        
+	      test1.addrecipe();
+	        
+	}
+
+}
