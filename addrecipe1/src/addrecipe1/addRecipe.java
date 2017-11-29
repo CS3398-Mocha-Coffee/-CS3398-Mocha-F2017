a87f61eed29572719d15501051766404208ed4ae
iabelh <aeh86@txstate.edu>
2017-11-29 16:27:24 -0600

    changed format to work with arthurs remove recipe

--- addrecipe1/src/addrecipe1/addRecipe.java
+++ addrecipe1/src/addrecipe1/addRecipe.java
@@ -4,8 +4,10 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,6 +16,7 @@ import java.util.Scanner;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+
 
 public class addRecipe {
 
@@ -33,35 +36,6 @@ public class addRecipe {
     
     void addrecipe() throws IOException
     {
-    	/*
-    	to handle future gui build
-    	listener = null; // to initialize listener
-        final int FIELD_WIDTH = 40; // adds the length of the button
-        frame1 = new JFrame(); 	// frame for 1 entry
-        frame1.setPreferredSize(new Dimension(1750,550)); // to change the size of the frame
-    	
-        text1 = new JTextField(FIELD_WIDTH); //to declare text box
-        text1.setFont(new Font("Arial", Font.PLAIN, 40)); 	// to make font that user enters bigger
-
-        button1 = new JButton("Enter Recipe Name.(press to Save)"); // button for user to enter 1 term
-        button1.setFont(new Font("Arial", Font.PLAIN, 40)); //to make text in button bigger and different size
-        
-    	
-        frame1.setLayout(new GridLayout(3, 30)); //makes the frame layout
-        frame1.add(button1); // to add button
-        frame1.add(text1);  // to add text box
-        
-        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // manages components to size them correctly in frames
-    	
-        frame1.pack();
-        //adds action listeners to all buttons in frames to make responsive
-    	
-        button1.addActionListener(listener);
-        // button2.addActionListener(listener);
-    	*/
-    	
-    	
     	File log = new File("User-Inputted-Recipes.csv");
     	        
         BufferedWriter file = null;
@@ -77,12 +51,18 @@ public class addRecipe {
         	//allows to append current file instead of overwrite
             file1 = new FileWriter(log, true); 
             file = new BufferedWriter(file1);
+            
+          
 
         //does while loop if you want enter new recipe. ends o/w
         while(response == 'y' || response == 'Y' )
         {
-            //frame1.setVisible(true);
-        	  	
+        	BufferedReader br = new BufferedReader(new FileReader("User-Inputted-Recipes.csv"));
+        	if (br.readLine() == null) {
+        		file.write("Name,Prep Time,Cook Time, Total Time,Ingredients,Measurement,Steps");
+        	}
+              	
+        	 
         	scan.nextLine(); //to fix last newline character error 
         	 
         	//asks for the name of the recipe
@@ -110,7 +90,8 @@ public class addRecipe {
             file.write(cookTime); 
             file.write(",");
             file.write(totalTime); 
-            file.newLine();
+            file.write(",");
+          
 
             //asks for amount of ingredients for recipe
             System.out.println("\nHow many ingredients are in this recipe?");
@@ -123,16 +104,21 @@ public class addRecipe {
             	
             	System.out.println("Enter ingredient number " + x );
             	ingredient = scan.nextLine();
-
-                System.out.println("Enter amount(will be based on type of ingredient)");
-                ingreAmount = scan.nextLine();
-
-                file.write(ingredient); // add ingredient to output file
-                file.write(",");
-                file.write(ingreAmount); 
-                file.newLine();
+            	
+            	file.write(ingredient); // add ingredient to output file
+            	file.write(":");
+            	
             }
-
+            file.write(",");
+            for(int y=1; y<=numIngre; y++)
+            {
+                System.out.println("Enter amount for ingredient #" + y);
+                ingreAmount = scan.nextLine();
+                file.write(ingreAmount); 
+            	file.write(":");
+            }
+            file.write(","); // skip cell for steps
+            
             // This adds directions to the recipes. 
             //asks for amount of number of directions
             System.out.println("\nHow many steps is this recipe?");
@@ -146,11 +132,10 @@ public class addRecipe {
             	direction = scan.nextLine();
 
                 file.write(direction); // adds directions to .csv file
-            	file.newLine();
+                file.write(":");
+                
             }
 
-            file.newLine();
-            
             //to print if user wants to enter another recipe & takes input
             System.out.print("To enter another recipe enter (y) to quit ");
             System.out.println("hit any other key.");
