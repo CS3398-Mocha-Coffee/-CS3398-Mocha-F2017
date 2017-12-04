d441b4a2d744f9c42402e2228c93ccf345b44605
iabelh <31448919+iabelh@users.noreply.github.com>
2017-12-04 01:59:01 -0600

    Add files via upload

---
+++ saverecipe1/src/saverecipe1/test.java
@@ -0,0 +1,14 @@
+package saverecipe1;
+
+import java.io.IOException;
+
+public class test {
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		  saveRecipee test1 = new saveRecipee();
+	        
+	      //test1.saveRecipeToCSV(); //to test saveRecipe file
+		  test1.readRecipesFrCSV();
+		
+	}
+}
